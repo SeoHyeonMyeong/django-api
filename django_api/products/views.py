b9fe63055e6ecd3edc3f73457655a4bccfeb7ab6
SeoHyeonMyeong <gusaud2199@naver.com>
2022-05-17 17:29:27 +0900

    update: Views are giving some contexts to Templates

--- django_api/products/views.py
+++ django_api/products/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request, *args, **kwargs):
-    return render(request, "index.html", {})
+    context = {
+        "user_name": "Seo",
+        "user_id": 123,
+        "user_comments": ["hi","what","thisis..."],
+    }
+    return render(request, "index.html", context)
