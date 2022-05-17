56473a189e064f74b1a0662347854deda212ee87
SeoHyeonMyeong <gusaud2199@naver.com>
2022-05-17 18:45:50 +0900

    Add: make product_detail_view and match urls

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from products.views import index_view
+from products.views import index_view, product_detail_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('product/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
--- django_api/products/views.py
+++ django_api/products/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
 def index_view(request, *args, **kwargs):
     context = {
@@ -9,3 +11,12 @@ def index_view(request, *args, **kwargs):
         "user_comments": ["hi","what","thisis..."],
     }
     return render(request, "index.html", context)
+
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    context = {
+        'title': obj.title,
+        'description': obj.description,
+        'price': obj.price,
+    }
+    return render(request, "product/detail.html", context)
