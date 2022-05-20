2b42388734f855ee8343e31ee01655d32c32fd6b
SeoHyeonMyeong <gusaud2199@naver.com>
2022-05-20 23:30:02 +0900

    Add: 아티클 카드뷰를 만들어서 인스타 앱에 접속시 자동으로 아티클 리스트를 가져와서 카드뷰를 생성하도록 설정!

--- django_api/instagram/views.py
+++ django_api/instagram/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.views import View
+
+from .models import Article
 
 # Create your views here.
 class InstagramView(View):
     def get(self, request):
-        return render(request, 'instagram/index.html')
+        obj = get_list_or_404(Article)
+        context = {
+            "articles": obj
+        }
+        return render(request, 'instagram/index.html', context)
         
