edbcfd368df3568ed1a802eae020454b3b403a6c
SeoHyeonMyeong <gusaud2199@naver.com>
2022-05-20 23:10:33 +0900

    Add: add article model to instagram app

--- django_api/instagram/models.py
+++ django_api/instagram/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    user        = models.CharField(max_length=20)
+    title       = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    pictures    = models.TextField(blank=True, null=True)
