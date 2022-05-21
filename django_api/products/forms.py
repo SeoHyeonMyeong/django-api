f2d2d11b1c2689e74b670a79bd5a54b08679687d
SeoHyeonMyeong <gusaud2199@naver.com>
2022-05-21 20:04:35 +0900

    Add: Footer Component

--- django_api/products/forms.py
+++ django_api/products/forms.py
@@ -7,6 +7,7 @@ class ProductForm(forms.ModelForm):
     title       = forms.CharField()
     description = forms.CharField()
     price       = forms.DecimalField()
+    
     class Meta:
         model = Product
         fields = [
